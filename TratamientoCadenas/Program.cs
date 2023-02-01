6516a9c5584185961220fbfe9de4798666f26532
AbilioJNS <116020016+AbilioJNS@users.noreply.github.com>
2023-02-01 09:36:57 +0100

    Añado proyectos

---
+++ TratamientoCadenas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TratamientoCadenas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce tu frase");
+            int aes = 0;
+            string frase = Console.ReadLine();
+            89574837 % 23 = 7
+            for (int i=0; i < frase.Length; i++)
+            {
+                if (frase[i].Equals('a') || frase[i].Equals('A')) {
+                    Console.WriteLine("aqui");
+                    aes++;
+                }
+            }
+            Console.WriteLine($"Tu frase tiene {aes} aes");
+        }
+    }
+}
---
+++ ValidarDNI/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ValidarDNI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("introduzca el dni a validar");
+                string dniEntero = Console.ReadLine();
+                string dniLetra = dniEntero.Substring(dniEntero.Length - 1);
+                string dniNumero = dniEntero.Substring(0, dniEntero.Length - 1);
+                string validarLetra = "TRWAGMYFPDXBNJZSQVHLCKE";
+                foreach(char c in validarLetra) 
+                {
+                    Console.WriteLine(c);
+                }
+                
+                if (dniLetra.Equals(Convert.ToString(validarLetra[calculaposicion(dniNumero)])))
+                {
+                    Console.WriteLine("dni Correcto");
+                    break;
+                }
+                else 
+                { 
+                    Console.WriteLine("letra dni "+dniLetra+" letra validada "+ validarLetra[calculaposicion(dniNumero)]);
+                    Console.WriteLine("Dni incorrecto");
+                }
+            }
+        }
+
+        private static int calculaposicion(string dniNumero)
+        {
+            
+            return int.Parse(dniNumero) % 23;
+        }
+    }
+}
